60dd3b49065493e569167a857e65902ba04e3aa2
danielkluchar <danielkluchar505@gmail.com>
2024-07-17 18:07:27 -0700

    Create hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,6 @@
+> git status
+> git add hello-world.py
+> git status 
+> git commit -m "created new file hello-world.py" # commits the files you just added
+> git push origin master # will push the code you committed to the remote server, master branch
+> git log # this will show you a history of your commits
